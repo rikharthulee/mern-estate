42af6d2ca39c0c7040b8302b7fd7fb3f5b811ad3
Richard Lee <141524775+rikharthulee@users.noreply.github.com>
2025-06-23 15:51:32 +0100

    Sign In route created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs"; // Import bcrypt for password hashing
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"; // Import jsonwebtoken for token generation
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -13,5 +14,26 @@ export const signup = async (req, res, next) => {
     });
   } catch (error) {
     next(error); // Pass the error to the next middleware (error handler)
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: _, ...userDetails } = validUser._doc; // Exclude the password from the user details
+
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+      message: "Login successful",
+      userDetails,
+    });
+  } catch (error) {
+    next(error);
   }
 };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { signup } from "../controllers/auth.controller.js"; // Import the signup controller from the auth controller module
+import { signin, signup } from "../controllers/auth.controller.js"; // Import the signup controller from the auth controller module
 
 const router = express.Router(); // Create a new Express router instance
 
 router.post("/signup", signup); // Define a POST route for user signup, using the signup controller
+router.post("/signin", signin); // Define a POST route for user login, using the signup controller (this should be replaced with the actual login controller)
 
 export default router; // Export the router to be used in other parts of the application
